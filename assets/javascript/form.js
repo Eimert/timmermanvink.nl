77536bbfa8dbce9ca2439612408f9e3526c06afb
Eimert Vink <eimertvink@gmail.com>
2020-06-13 20:45:38 +0200

    Optimized the contact form usage

--- assets/javascript/form.js
+++ assets/javascript/form.js
@@ -1,32 +1,9 @@
-
-document.addEventListener("load", addListeners());
-
-function addListeners() {
-    let form = document.querySelector("#contactForm");
-    form.addEventListener('submit', handleForm);
-}
+document.querySelector("#contactForm").addEventListener('submit', handleForm);
 
 function handleForm(event) {
     // event.preventDefault();
     console.log("submit clicked");
-    
-    // document.querySelector("form").remove();
-    // document.querySelector("button[type=submit]").disabled = true
-
-    document.querySelector(".lead").textContent = "Bedankt voor uw bericht, over het algemeen hoort u van ons binnen 48 uur."
+    document.querySelector("#contactForm").style.display = "none";
+    document.querySelector("#dummyframe").style.display = "inline";
     // return false;
 }
-
-// todo: handle form with ajax to prevent reloading the page
-// $(function() {
-//     $('form').submit(function() {
-//         $.ajax({
-//             type: 'POST',
-//             url: 'submit.php',
-//             data: { username: $(this).name.value, 
-//                     password: $(this).password.value }
-//         });
-//         return false;
-//     }); 
-// })
-
