57495febfcc37ef091086fb8d52c4c9b64838f18
Eimert Vink <eimertvink@gmail.com>
2020-06-14 16:03:50 +0200

    Uncaught TypeError: a is null

---
+++ assets/javascript/gallery.js
@@ -0,0 +1,17 @@
+const pswpElement = document.querySelector('.pswp'),
+    items = [
+        {
+            src: 'https://placekitten.com/600/400',
+            w: 600,
+            h: 400
+        },
+        {
+            src: 'https://placekitten.com/1200/900',
+            w: 1200,
+            h: 900
+        }
+    ],
+    options = { index: 0 },
+    gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+
+gallery.init();
