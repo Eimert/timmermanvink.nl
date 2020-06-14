543f84adb52af9aefddfbcbccdfb96472701887a
Eimert Vink <eimertvink@gmail.com>
2020-06-14 14:23:10 +0200

    loader animation after form submit

--- assets/javascript/form.js
+++ assets/javascript/form.js
@@ -4,6 +4,13 @@ function handleForm(event) {
     // event.preventDefault();
     console.log("submit clicked");
     document.querySelector("#contactForm").style.display = "none";
-    document.querySelector("#dummyframe").style.display = "inline";
+    document.querySelector(".loader").style.display = "block";
+
+    setTimeout(function () {
+        // hide loader
+        document.querySelector(".loader").style.display = "none";
+        // show
+        document.querySelector("#dummyframe").style.display = "inline";
+    }, 4000);
     // return false;
 }
