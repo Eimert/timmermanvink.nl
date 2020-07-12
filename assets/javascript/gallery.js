fe576b1595ec2747127eaf59cf334780e750c8c3
Eimert Vink <eimertvink@gmail.com>
2020-07-12 02:49:20 +0200

    pswp element still null - why?

--- assets/javascript/gallery.js
+++ assets/javascript/gallery.js
@@ -1,4 +1,3 @@
-// todo: fix photoswipe
 // const pswpElement = document.querySelector('.pswp'),
 //     items = [
 //         {
@@ -16,3 +15,31 @@
 //     gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
 
 // gallery.init();
+
+
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// build items array
+var items = [
+    {
+        src: 'https://placekitten.com/600/400',
+        w: 600,
+        h: 400
+    },
+    {
+        src: 'https://placekitten.com/1200/900',
+        w: 1200,
+        h: 900
+    }
+];
+
+// define options (if needed)
+var options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
